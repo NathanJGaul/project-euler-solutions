8cf64c46ae7dc94360a2002cd4176b0662c7834c
Nathan Gaul <nathanjgaul@gmail.com>
2021-02-15 19:45:28 -0500

    Problem 10

---
+++ src/problem10.ts
@@ -0,0 +1,16 @@
+function primesBelow(n: number): Array<number> {
+    let sieve: boolean[] = Array.from({length: n}, (_, __) => true);
+    let primes: Array<number> = [];
+    for (let p = 2; p < sieve.length; ++p) {
+        if (sieve[p]) {
+            primes.push(p);
+            for (let i = p+p; i < sieve.length; i+=p) {
+                sieve[i] = false;
+            }
+        }
+    }
+    return primes;
+}
+
+let sum: number = primesBelow(2000000).reduce((acc, cur) => acc + cur);
+console.log(sum);
